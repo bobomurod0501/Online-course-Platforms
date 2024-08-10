1c05d3ba5946cb9904c4529de06b7dabb89739d0
Bobomurod <bobomurodeshmamatov@gmail.com>
2024-08-10 15:34:09 +0500

    updates

--- src/components/HtmlTestPage.jsx
+++ src/components/HtmlTestPage.jsx
@@ -7,11 +7,8 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { useRef } from "react";
 import { useContext } from "react";
 import { contextProvider } from "../provider/MainProvider";
-
-
 
 const fetchQuizTestData = async () => {
   return await axios.get("http://localhost:3000/quizzes");
@@ -19,10 +16,14 @@ const fetchQuizTestData = async () => {
 
 function HtmlTestPage() {
   const [questionNum, setQuestionNum] = useState(0);
-  const[result, setResult] = useState('')
-  const [isPressed, setIsPressed] = useState(false)
-  const states = useContext(contextProvider)
-  const {trueAnswer, setTrueAnswer, testData, setTestData} = states
+  const [result, setResult] = useState("");
+  const [isPressed, setIsPressed] = useState(false);
+  const states = useContext(contextProvider);
+  const { trueAnswer, setTrueAnswer, testData, setTestData } = states;
+  const [variantA, setVariantA] = useState(false);
+  const [variantB, setVariantB] = useState(false);
+  const [variantC, setVariantC] = useState(false);
+  const [variantD, setVariantD] = useState(false);
 
   // console.log(trueAnswer)
   const params = useParams();
@@ -44,7 +45,6 @@ function HtmlTestPage() {
     return <h1>{error.message}</h1>;
   }
 
-  // console.log(data)
 
   const newData = data?.data.filter(
     (item) => item.title.toLowerCase() == params.testId
@@ -52,32 +52,62 @@ function HtmlTestPage() {
   // console.log(newData);
 
   const submitButtonFunc = () => {
-    setTestData(newData)
-    if(isPressed){
-      setQuestionNum(questionNum ==9 ? 9 && navigate("resultpage")  : questionNum + 1);
-      questionNum == 9 ? setIsPressed(true) : setIsPressed(false)
-    }else{
-      alert("please select answer")
-      setQuestionNum(questionNum)
+    setTestData(newData);
+    if (isPressed) {
+      setQuestionNum(
+        questionNum == 9 ? 9 && navigate("resultpage") : questionNum + 1
+      );
+      questionNum == 9 ? setIsPressed(true) : setIsPressed(false);
+    } else {
+      alert("please select answer");
+      setQuestionNum(questionNum);
     }
-    
-  };
-  const checkTestAnswer = (val) => {
-    if(newData[0].questions[questionNum].answer == val){
-      setResult(true)
-      setTrueAnswer(trueAnswer + 1)
-    }else{
-      setResult(false)
+    if(result){
+      setTrueAnswer(trueAnswer + 1);
     }
-    setIsPressed(true)
 
+    setVariantA(false);
+    setVariantB(false);
+    setVariantC(false);
+    setVariantD(false);
+  };
+  const checkTestAnswer = (val, variant) => {
+    if (newData[0].questions[questionNum].answer == val) {
+      setResult(true);
+      
+    } else {
+      setResult(false);
+    }
+    setIsPressed(true);
 
-
+    if (variant == "A") {
+      setVariantA(true);
+      setVariantB(false);
+      setVariantC(false);
+      setVariantD(false);
+    } else if (variant == "B") {
+      setVariantB(true);
+      setVariantA(false);
+      setVariantC(false);
+      setVariantD(false);
+    } else if (variant == "C") {
+      setVariantC(true);
+      setVariantA(false);
+      setVariantB(false);
+      setVariantD(false);
+    } else {
+      setVariantD(true);
+      setVariantA(false);
+      setVariantB(false);
+      setVariantC(false);
+    }
   };
 
-  console.log(result)
+  console.log(result);
 
-  
+  const borderColor = {
+    border: "3px solid blue",
+  };
 
   return (
     <div className="testPageBox" style={{ height: "90vh" }}>
@@ -110,40 +140,41 @@ function HtmlTestPage() {
         </div>
         <div className="testBtnBox">
           <button
+            // style={}
             value={`${newData[0].questions[questionNum].options[0]}`}
-            onClick={(e) => checkTestAnswer(e.target.value)}
-            className="testBtn"
+            onClick={(e) => checkTestAnswer(e.target.value, "A")}
+            className={`testBtn ${variantA ? "borderColor" : ""}`}
           >
-            <span>A</span> {newData[0].questions[questionNum].options[0]}
+            <span className={`${variantA ? "variantbackColor" : ""}`}>A</span>{" "}
+            {newData[0].questions[questionNum].options[0]}
           </button>
           <button
             value={`${newData[0].questions[questionNum].options[1]}`}
-            onClick={(e) => checkTestAnswer(e.target.value)}
-            className="testBtn"
-            // style={isPressed ? {border: "3px solid blue"} : ""}
+            onClick={(e) => checkTestAnswer(e.target.value, "B")}
+            className={`testBtn ${variantB ? "borderColor" : ""}`}
           >
-            <span>B</span> {newData[0].questions[questionNum].options[1]}
+            <span className={`${variantB ? "variantbackColor" : ""}`}>B</span>{" "}
+            {newData[0].questions[questionNum].options[1]}
           </button>
           <button
             value={`${newData[0].questions[questionNum].options[2]}`}
-            onClick={(e) => checkTestAnswer(e.target.value)}
-            className="testBtn"
+            onClick={(e) => checkTestAnswer(e.target.value, "C")}
+            className={`testBtn ${variantC ? "borderColor" : ""}`}
             // style={isPressed ? {border: "3px solid blue"} : ""}
           >
-            <span>C</span> {newData[0].questions[questionNum].options[2]}
+            <span className={`${variantC ? "variantbackColor" : ""}`}>C</span>{" "}
+            {newData[0].questions[questionNum].options[2]}
           </button>
           <button
             value={`${newData[0].questions[questionNum].options[3]}`}
-            onClick={(e) => checkTestAnswer(e.target.value)}
-            className="testBtn"
+            onClick={(e) => checkTestAnswer(e.target.value, "D")}
+            className={`testBtn ${variantD ? "borderColor" : ""}`}
             // style={isPressed ? {border:' 3px solid blue'} : ""}
           >
-            <span>D</span> {newData[0].questions[questionNum].options[3]}
+            <span className={`${variantD ? "variantbackColor" : ""}`}>D</span>{" "}
+            {newData[0].questions[questionNum].options[3]}
           </button>
-          <button
-            onClick={() => submitButtonFunc()}
-            className="testSubmitBtn"
-          >
+          <button onClick={() => submitButtonFunc()} className="testSubmitBtn">
             {" "}
             {questionNum == 9 ? "Submit" : "Next"}
           </button>
